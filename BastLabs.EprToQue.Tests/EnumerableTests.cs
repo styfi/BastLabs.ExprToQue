022a013719f63917a1723c3c046312d105880450
Michal Beňo <michal.beno@puxdesign.cz>
2021-10-10 18:00:17 +0200

    Format unit tests

--- BastLabs.EprToQue.Tests/EnumerableTests.cs
+++ BastLabs.EprToQue.Tests/EnumerableTests.cs
@@ -13,7 +13,8 @@ namespace BastLabs.EprToQue.Tests
         public void IntListContains()
         {
             IEnumerable<int> intList = new List<int>() { 1, 2, 3 };
-            Expression<Func<Product, bool>> expr = p => intList.Contains(p.ID);
+            Expression<Func<Product, bool>> expr;
+            expr = p => intList.Contains(p.ID);
 
             Assert.AreEqual("(ID IN (1,2,3))", ExprToQueService.Translate(expr));
         }
@@ -22,7 +23,8 @@ namespace BastLabs.EprToQue.Tests
         public void IntListNotContains()
         {
             IEnumerable<int> intList = new List<int>() { 1, 2, 3 };
-            Expression<Func<Product, bool>> expr = p => !intList.Contains(p.ID);
+            Expression<Func<Product, bool>> expr;
+            expr = p => !intList.Contains(p.ID);
 
             Assert.AreEqual(" NOT (ID IN (1,2,3))", ExprToQueService.Translate(expr));
         }
@@ -31,7 +33,8 @@ namespace BastLabs.EprToQue.Tests
         public void EmptyIntListContains()
         {
             IEnumerable<int> intList = new List<int>();
-            Expression<Func<Product, bool>> expr = p => intList.Contains(p.ID);
+            Expression<Func<Product, bool>> expr;
+            expr = p => intList.Contains(p.ID);
 
             Assert.AreEqual("(ID IN (SELECT 1 WHERE 1=0))", ExprToQueService.Translate(expr));
         }
@@ -49,7 +52,8 @@ namespace BastLabs.EprToQue.Tests
         public void StringListContains()
         {
             IEnumerable<string> stringList = new List<string>() { "a", "b", "c" };
-            Expression<Func<Product, bool>> expr = p => stringList.Contains(p.Name);
+            Expression<Func<Product, bool>> expr;
+            expr = p => stringList.Contains(p.Name);
 
             Assert.AreEqual("(Name IN ('a','b','c'))", ExprToQueService.Translate(expr));
         }
@@ -58,7 +62,8 @@ namespace BastLabs.EprToQue.Tests
         public void StringListNotContains()
         {
             IEnumerable<string> stringList = new List<string>() { "a", "b", "c" };
-            Expression<Func<Product, bool>> expr = p => !stringList.Contains(p.Name);
+            Expression<Func<Product, bool>> expr;
+            expr = p => !stringList.Contains(p.Name);
 
             Assert.AreEqual(" NOT (Name IN ('a','b','c'))", ExprToQueService.Translate(expr));
         }
@@ -67,7 +72,8 @@ namespace BastLabs.EprToQue.Tests
         public void EmptyStringListContains()
         {
             IEnumerable<string> stringList = new List<string>();
-            Expression<Func<Product, bool>> expr = p => stringList.Contains(p.Name);
+            Expression<Func<Product, bool>> expr;
+            expr = p => stringList.Contains(p.Name);
 
             Assert.AreEqual("(Name IN (SELECT 1 WHERE 1=0))", ExprToQueService.Translate(expr));
         }
@@ -76,7 +82,8 @@ namespace BastLabs.EprToQue.Tests
         public void EmptyStringListNotContains()
         {
             IEnumerable<string> stringList = new List<string>();
-            Expression<Func<Product, bool>> expr = p => !stringList.Contains(p.Name);
+            Expression<Func<Product, bool>> expr;
+            expr = p => !stringList.Contains(p.Name);
 
             Assert.AreEqual(" NOT (Name IN (SELECT 1 WHERE 1=0))", ExprToQueService.Translate(expr));
         }
